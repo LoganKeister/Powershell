13043f917a1c840269eb9ef56d43b8faebc76b62
LoganKeister <120484023+LoganKeister@users.noreply.github.com>
2023-01-03 17:27:02 -0500

    Update FindActiveUsers.ps1

--- FindActiveUsers.ps1
+++ FindActiveUsers.ps1
@@ -1,1 +1,4 @@
+# This script can be run against a computer and can query the active user. One only needs to change
+# the HOSTNAME to whatever the computer name is.
+
 Get-WmiObject –ComputerName HOSTNAME –Class Win32_ComputerSystem | Select-Object UserName
